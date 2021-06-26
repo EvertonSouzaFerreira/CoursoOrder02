4709ac0197299987503292392029331b145ebb69
Everton Ferreira <everton.souzaferreira@outlook.com>
2021-06-26 12:21:03 +0200

    Adicionar arquivos de projeto.

---
+++ CoursoOrder02/Entitys/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursoOrder02.Entitys
+{
+    class Product
+    {
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string productName, double price)
+        {
+            ProductName = productName;
+            Price = price;
+        }
+    }
+}
---
+++ CoursoOrder02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CoursoOrder02.Entitys.Enums;
+using CoursoOrder02.Entitys;
+
+namespace CoursoOrder02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item date: ");
+                Console.Write("Product name:");
+                string producName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine());               
+
+                Product product = new Product(producName, price);
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine("Order summary:");
+            Console.WriteLine(order);
+
+            
+        }
+    }
+}
